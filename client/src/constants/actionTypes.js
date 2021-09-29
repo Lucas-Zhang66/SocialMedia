3a07c865939485a13d1d65ec849460091b7c3250
Lucas-Zhang66 <67560714+Lucas-Zhang66@users.noreply.github.com>
2021-09-29 00:42:02 -0400

    Add files via upload

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,7 @@
+export const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const CREATE = 'CREATE';
+export const FETCH_SEARCH = 'FETCH_SEARCH';
+export const START_LOADING = 'START_LOADING';
+export const END_LOADING = 'END_LOADING';
+export const COMMENT = 'COMMENT';
